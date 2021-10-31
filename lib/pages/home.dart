da2a4455df52dcb3b36e4cfa5ea68c3ff40aa251
Денис Веркошанский <max07121997@gmail.com>
2021-10-31 20:19:08 +0300

    Add addbutton
    добавлена кнопка добавления новой заметки, а так же кнопка удаления

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,6 +8,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String userToDo = '';
   List zamlist = [];
 
   @override
@@ -20,30 +21,99 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: Text(
-            'Заметки',
-            style: TextStyle(color: Colors.orange[700]),
-          ),
-          backgroundColor: Colors.black,
-          centerTitle: true,
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text(
+          'Заметки',
+          style: TextStyle(color: Colors.orange[700]),
         ),
-        body: ListView.builder(
-          itemCount: zamlist.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Dismissible(
-                key: Key(zamlist[index]),
-                child: Card(
-                  color: Colors.grey[800],
-                  child: ListTile(
-                    title: Text(
-                      zamlist[index],
-                      style: TextStyle(color: Colors.white),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: zamlist.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Dismissible(
+            key: Key(zamlist[index]),
+            child: Card(
+              color: Colors.grey[800],
+              child: ListTile(
+                title: Text(
+                  zamlist[index],
+                  style: TextStyle(color: Colors.white),
+                ),
+                trailing: IconButton(
+                    // ignore: prefer_const_constructors
+                    icon: Icon(
+                      Icons.delete_forever,
+                      color: Colors.orange,
                     ),
+                    onPressed: () {
+                      setState(() {
+                        zamlist.removeAt(index);
+                      });
+                    }),
+              ),
+            ),
+            onDismissed: (direction) {
+              //if(direction == DismissDirection.endToStart )
+              setState(() {
+                zamlist.removeAt(index);
+              });
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey[800],
+        child: Icon(
+          Icons.add,
+          color: Colors.orange,
+        ),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                const kOrange = const Color(0xFFffd600);
+                return AlertDialog(
+                  backgroundColor: Colors.grey[800],
+                  title: Text(
+                    'Новая заметка',
+                    style: TextStyle(color: Colors.orange[700]),
                   ),
-                ));
-          },
-        ));
+                  content: TextField(
+                    style: TextStyle(color: Colors.white),
+                    cursorColor: Colors.orange,
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.orange)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                const BorderSide(color: Colors.orange))),
+                    onChanged: (String value) {
+                      userToDo = value;
+                    },
+                  ),
+                  actions: [
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            zamlist.add(userToDo);
+                            Navigator.of(context).pop();
+                          });
+                        },
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color?>(
+                                Colors.grey[700])),
+                        child: Text(
+                          'добавить',
+                          style: TextStyle(color: Colors.orange[700]),
+                        ))
+                  ],
+                );
+              });
+        },
+      ),
+    );
   }
 }
