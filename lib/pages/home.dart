ac4335b70c390bab5aa5e96910a459914848a0e3
Денис Веркошанский <max07121997@gmail.com>
2021-10-31 18:45:53 +0300

    List add
    добавлен лист для заметок на главную панель

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List zamlist = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    zamlist.addAll(['артур', 'не артур', 'куда же без крепления']);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text(
+            'Заметки',
+            style: TextStyle(color: Colors.orange[700]),
+          ),
+          backgroundColor: Colors.black,
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: zamlist.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+                key: Key(zamlist[index]),
+                child: Card(
+                  color: Colors.grey[800],
+                  child: ListTile(
+                    title: Text(
+                      zamlist[index],
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ));
+          },
+        ));
   }
 }
